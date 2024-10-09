b0d3a537924eac9c10f1e4bcff6a9f1c1964f1ca
Luiz Fernando <luiz-fernando@outlook.com>
2024-10-09 16:48:21 -0300

    Criado app candidato
    Close #3

--- core/candidato/apps.py
+++ core/candidato/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EmpresaConfig(AppConfig):
+class CandidatoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core.empresa'
+    name = 'core.candidato'
--- core/settings.py
+++ core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'empresa',
+    'core.empresa',
+    'core.candidato',
 ]
 
 MIDDLEWARE = [
