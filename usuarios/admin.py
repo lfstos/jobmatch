0ecdb6fc25206e0acef92049b2b83e23fcc760f9
Luiz Fernando <luiz-fernando@outlook.com>
2024-12-02 16:43:38 -0300

    Configurado seções do ruff no pyproject
    close #24

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,3 +1,3 @@
-from django.contrib import admin  # NOQA
+from django.contrib import admin #noqa
 
 # Register your models here.
--- usuarios/models.py
+++ usuarios/models.py
@@ -93,6 +93,3 @@ class User(AbstractUser, PermissionsMixin):
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
         send_mail(subject, message, from_email, [self.email], **kwargs)
-
-
-teste = "kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
