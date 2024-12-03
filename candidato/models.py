a705543d1d11446f732e1c508c9da96cf25c2183
Luiz Fernando <luiz-fernando@outlook.com>
2024-12-03 19:03:31 -0300

    Criado modelo candidato
    close #5

---
+++ candidato/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from choices.choices import get_faixa_salarial_choices, get_escolaridade_choices
+from empresa.models import Vaga
+from usuarios.models import User
+
+
+class Candidato(models.Model):
+    email = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    # nome = models.CharField(max_length=100)
+    faixa_salarial = models.CharField(max_length=5, choices=get_faixa_salarial_choices)
+    escolaridade = models.CharField(max_length=18, choices=get_escolaridade_choices)
+    experiencia = models.TextField()
+    vaga = models.ForeignKey(Vaga, on_delete=models.DO_NOTHING)
