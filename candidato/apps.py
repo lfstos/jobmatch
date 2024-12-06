ed079b217505de6721271f428eec03f7b3845bca
Luiz Fernando <luiz-fernando@outlook.com>
2024-12-06 15:05:53 -0300

    Refatorado app Candidato
    close #36

--- candidato/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CandidatoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'candidato'
--- core/settings.py
+++ core/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
     'usuarios',
     'empresa',
-    'candidato',
     'vagas',
 ]
 
