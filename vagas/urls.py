cb6f1c89773c627df5da6581c99b529d6317c23d
Luiz Fernando <luiz-fernando@outlook.com>
2024-12-12 20:34:06 -0300

    Criado testes de edição e exclusão de vagas

--- vagas/urls.py
+++ vagas/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('cadastrar_vagas/', views.cadastrar_vagas, name='cadastrar_vagas'),
     path('lista_vagas/', views.lista_vagas, name='lista_vagas'),
     path('candidatar_vaga', views.candidatar_vaga, name='candidatar_vaga'),
+    path('editar_vaga', views.editar_vaga, name='editar_vaga'),
+    path('excluir_vaga', views.excluir_vaga, name='excluir_vaga'),
 ]
