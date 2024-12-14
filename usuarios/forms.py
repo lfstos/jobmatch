1734453cf18d1fc745030b093effdb922a7204c8
Luiz Fernando <luiz-fernando@outlook.com>
2024-12-14 19:56:51 -0300

    Criada tela de login
    close #16

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -2,5 +2,12 @@ from django import forms
 
 
 class UsuarioForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput(), label='Senha', )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+        label=''
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Senha'}),
+        label='Senha'
+    )
