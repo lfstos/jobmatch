93a8e7d21aa63d5d5c02344faae2f1fbbabad4a1
Luiz Fernando <luiz-fernando@outlook.com>
2024-12-14 19:56:51 -0300

    Criada tela de login
    close #16

--- usuarios/test_models.py
+++ usuarios/test_models.py
@@ -1,7 +1,6 @@
 import pytest
 from django.test import Client
 from .models import User
-from django.urls import reverse
 
 @pytest.mark.django_db
 def test_cria_candidato():
@@ -40,6 +39,8 @@ def test_cria_usuario_sem_email():
 
 def test_home():
     client = Client()
-
-    url = reverse('home')
-    client.get(url)
+    response = client.get('/')
+    assert response.status_code == 200
+    # assert 'home' in response.home.html
+    
+        
